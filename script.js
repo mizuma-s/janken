32b8188a10660e42d9dc0ac54edfa149af5f8265
mizuma <c1106075@planet.kanazawa-it.ac.jp>
2025-01-15 17:54:23 +0900

    teachable machineのライブラリ修正とjavaScript修正

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const Model_URL = "models/pose/";
+
+let model, maxPredictions;
+
+window.onload = async function () {
+  console.log("ページが読み込まれました。");
+  if (typeof tmPose === "undefined") {
+    console.error("tmPoseが未定義です。スクリプトが正しくロードされているか確認してください。");
+    return;
+  }
+  await init();
+}
+
+async function init() {
+  try {
+    console.log("モデルのロード開始...");
+    model = await tmPose.load(Model_URL + "model.json", Model_URL + "metadata.json");
+    console.log("モデルロード完了");
+
+    if (model) {
+      maxPredictions = model.getTotalClasses();
+      console.log("クラス数:", maxPredictions);
+    } else {
+      console.error("モデルがロードされませんでした。");
+    }
+  } catch (error) {
+    console.error("モデルのロード中にエラーが発生しました。:", error);
+  }
+}
+
 async function startCamera() {
   const videoElement = document.getElementById("webcam");
   try {
